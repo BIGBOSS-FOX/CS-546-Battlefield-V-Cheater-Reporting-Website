cd319374f3f27102224b288eb2ce4e4cbee0e224
unknown <chendymaodai@gmail.com>
2019-05-06 15:51:13 -0400

    updated create report in routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const usersData = data.Users;
+const reportsData = data.Report;
 
 router.get("/:id", async(req, res) => {
     try {
@@ -65,6 +66,54 @@ router.put("/", async(req, res) => { //this is the route for updating the databa
     try {
         //this updates a user's status (can be generalized to update anything else if need be)
         const updatedUser = usersData.updateUser(req.body.id, req.body) //for now req.body is just userId and status but can be more
+        res.render('layouts/example', { data: updatedUser });
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+//Reported you received will be posted here
+router.post("/:id", async(req, res) => {
+    const reportInfo = req.body;
+    console.log(reportInfo);
+
+    if (!reportInfo) {
+        res.status(400).json({ error: "You must provide data to add a report" });
+        return;
+    }
+
+    if (!reportInfo.userID) {
+        res.status(400).json({ error: "You must provide a user ID" });
+        return;
+    }
+
+    if (!reportInfo.exampleFormControlTextarea1) {
+        res.status(400).json({ error: "You must provide an evidence" });
+        return;
+    }
+
+
+    try {
+        //add a new report to Report collection
+        const newReport = await reportsData.addReport(req.session.userlogged.user_name, reportInfo.userID, reportInfo.exampleFormControlTextarea1, reportInfo.exampleFormControlFile1, reportInfo.link);
+        
+        //get the reported_player info, add newReport to received_reports array, then update user info to database
+        const reportedPlayerInfo = await usersData.findUserByUserName(reportInfo.userID);
+        reportedPlayerInfo.received_reports.push(newReport);
+        const updatedReportedPlayer = await usersData.updateUser(reportedPlayerInfo._id, reportedPlayerInfo);
+        console.log(updatedReportedPlayer);
+
+        //get the reported_by info, add newReport to created_reports array, then update user info to database
+        const reportPlayerInfo = await usersData.findUserByUserName(req.session.userlogged.user_name);
+        reportPlayerInfo.created_reports.push(newReport);
+        const updatedReportPlayer = await usersData.updateUser(reportPlayerInfo._id, reportPlayerInfo);
+        console.log(updatedReportPlayer);
+
+        //res.redirect("/:id")
+
+
+
         res.render('layouts/example', { data: updatedUser });
     } catch (e) {
         console.log(e);
