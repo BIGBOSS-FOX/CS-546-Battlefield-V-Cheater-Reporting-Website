dc3d51ae1c6a27bac86f3fc20d8767c2e3529308
Tolga Yavuz <tolgayavuz@Tolgas-MacBook-Pro.local>
2019-05-10 20:06:30 -0400

    polls

--- routes/polls.js
+++ routes/polls.js
@@ -6,86 +6,74 @@ const reportsData = data.Report;
 const usersData = data.Users;
 const { ObjectId } = require('mongodb').ObjectID;
 //we may not need any routes for this
-router.get("/", async (req, res) => {
-    try
-    {
+router.get("/", async(req, res) => {
+    try {
         let pollMessage = false;
         let pollsList = await pollsData.getAllPolls();
         //let n=0; 
         //pollsList.reportedinfo ={};
-        for (var j = 0; j < pollsList.length; j++) 
-        {
-            let reportedinfo = await reportsData.getAllReceivedReportsByReportedPlayer(pollsList[j].voting_about);  
+        for (var j = 0; j < pollsList.length; j++) {
+            let reportedinfo = await reportsData.getAllReceivedReportsByReportedPlayer(pollsList[j].voting_about);
             console.log(reportedinfo);
-            if (reportedinfo != undefined && reportedinfo != null) 
-                {
-                    pollMessage = true;                   
-                }
-                pollsList[j]["reportedinfo"] = reportedinfo;  
-                console.log(pollsList);         
+            if (reportedinfo != undefined && reportedinfo != null) {
+                pollMessage = true;
+            }
+            pollsList[j]["reportedinfo"] = reportedinfo;
+            console.log(pollsList);
         }
         console.log(pollsList);
-        res.render('layouts/polls', { data : pollsList, hasdata : pollMessage });
-    }
-    catch (e) 
-    {
+        res.render('layouts/polls', { data: pollsList, hasdata: pollMessage });
+    } catch (e) {
         req.session.userlogged = null;
         res.clearCookie("AuthCookie");
-        res.status(404).render("layouts/error", {errors: e , layout: 'errorlayout' });
+        res.status(404).render("layouts/error", { errors: e, layout: 'errorlayout' });
     }
 });
 
 
-router.get("/:username", async (req, res) => {
-    try
-    {
+router.get("/:username", async(req, res) => {
+    try {
         let pollMessage = false;
         let dummy = await pollsData.getAllPolls();
         let pollsList = [];
-        for (var j = 0; j < dummy.length; j++) 
-        {
+        for (var j = 0; j < dummy.length; j++) {
             console.log(dummy[j]);
 
-            if(dummy[j].voting_about == req.params.username){
+            if (dummy[j].voting_about == req.params.username) {
                 pollsList.push(dummy[j]);
             }
-        }    
-        
-        for (var j = 0; j < pollsList.length; j++) 
-        {
-            let reportedinfo = await reportsData.getAllReceivedReportsByReportedPlayer(pollsList[j].voting_about);  
+        }
+
+        for (var j = 0; j < pollsList.length; j++) {
+            let reportedinfo = await reportsData.getAllReceivedReportsByReportedPlayer(pollsList[j].voting_about);
             console.log(reportedinfo);
-            if (reportedinfo != undefined && reportedinfo != null) 
-                {
-                    pollMessage = true;                   
-                }
-                pollsList[j]["reportedinfo"] = reportedinfo;  
-                console.log(pollsList);         
+            if (reportedinfo != undefined && reportedinfo != null) {
+                pollMessage = true;
+            }
+            pollsList[j]["reportedinfo"] = reportedinfo;
+            console.log(pollsList);
         }
         console.log(pollsList);
-        res.render('layouts/polls', { data : pollsList, hasdata : pollMessage });
-    }
-    catch (e) 
-    {
+        res.render('layouts/polls', { data: pollsList, hasdata: pollMessage });
+    } catch (e) {
         req.session.userlogged = null;
         res.clearCookie("AuthCookie");
-        res.status(404).render("layouts/error", {errors: e , layout: 'errorlayout' });
+        res.status(404).render("layouts/error", { errors: e, layout: 'errorlayout' });
     }
 });
 
 
-router.post("/:id", async (req, res) => {
-    try{
+router.post("/:id", async(req, res) => {
+    try {
         //we need a function to update a poll for voting about a userID 
         const vote = "vote casted!";
         res.render('layouts/example', { data: vote });
-    }
-    catch (e) 
-    {
+    } catch (e) {
         req.session.userlogged = null;
         res.clearCookie("AuthCookie");
-        res.status(404).render("layouts/error", {errors: e , layout: 'errorlayout' });
+        res.status(404).render("layouts/error", { errors: e, layout: 'errorlayout' });
     }
 });
+
 
 module.exports = router;
