00d12a09b907e4a040174c2c3fb0492838eb228a
codankra <codankra@gmail.com>
2019-05-10 20:06:28 -0400

    Add poll post logic

--- data/Users.js
+++ data/Users.js
@@ -195,5 +195,4 @@ module.exports = {
             }
         }
     }
-
 };
--- routes/polls.js
+++ routes/polls.js
@@ -7,8 +7,7 @@ const usersData = data.Users;
 const { ObjectId } = require('mongodb').ObjectID;
 //we may not need any routes for this
 router.get("/", async (req, res) => {
-    try
-    {
+    try {
         let pollMessage = false;
         let pollsList = await pollsData.getAllPolls();
         //let n=0; 
@@ -27,8 +26,7 @@ router.get("/", async (req, res) => {
         console.log(pollsList);
         res.render('layouts/polls', { data : pollsList, hasdata : pollMessage });
     }
-    catch (e) 
-    {
+    catch (e) {
         req.session.userlogged = null;
         res.clearCookie("AuthCookie");
         res.status(404).render("layouts/error", {errors: e , layout: 'errorlayout' });
@@ -77,9 +75,26 @@ router.get("/:username", async (req, res) => {
 router.post("/", async (req, res) => {
     try{
         //we need a function to update a poll for voting about a userID
-        
-        const vote = "vote casted!";
-        res.render('layouts/example', { data: vote });
+        const voteinfo = req.body;
+        const FullPoll = pollsData.getPollByObjectId(voteinfo.id);
+        //if already voted: error
+        let adminExists = false;
+        const votesArray = FullPoll.votes;
+        //check if admin exists in vote list
+        for (let i = 0; i <votesArray.length; i++){
+            if (votesArray[i].admin == req.session.userlogged.user_name) {
+                adminExists = true;
+                break;
+            }
+        }
+        if(adminExists) throw "One admin cannot vote on the same poll twice";
+
+        pollsData.addVoteToPoll(FullPoll.voting_about, req.session.userlogged.user_name, voteinfo.vote);
+        if(FullPoll.votes.length === 3){
+            usersData.statusChange(FullPoll.voting_about);
+            pollsData.deletePoll(req.body.id);
+        }
+        res.render("layouts/main", {users: user});
     }
     catch (e) 
     {
@@ -88,5 +103,8 @@ router.post("/", async (req, res) => {
         res.status(404).render("layouts/error", {errors: e , layout: 'errorlayout' });
     }
 });
+//get admin info, vote info, poll id
+
+
 
 module.exports = router;
